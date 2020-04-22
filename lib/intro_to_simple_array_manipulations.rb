887a2f68628e5156ae0cba1dfff12a52ec31d874
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-22 19:27:00 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push (array, string)
+  array.push(string)
+end 
+
+def using_unshift (arr, str)
+  arr.unshift(str)
+end 
+
+def using_pop (arr)
+  arr.pop()
+end 
+
+def pop_with_args (arr)
+  arr.pop(2)
+end 
+
+def using_shift (arr)
+  arr.shift()
+end
+
+def shift_with_args (arr)
+  arr.shift(2)
+end
